ec78ea59f9eaea94a7befc1d60ec026779a64cf0
MauriiWhite <mauriblanco.s@gmail.com>
2025-07-28 23:41:10 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <h1 className="text-3xl font-bold text-slate-100">
+        Tauri Desk Test 0.1.0
+      </h1>
+    </div>
+  );
+}
+
+export default App;
